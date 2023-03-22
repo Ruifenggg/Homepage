795ff34650f091700f67086e51d4f734e6599657
Ruifeng Liang <111317718+Ruifenggg@users.noreply.github.com>
2023-03-22 16:37:56 +0000

    Wed, Mar 22, 2023, 4:37 PM +00:00

--- js/colorboard.js
+++ js/colorboard.js
@@ -1,2 +1,21 @@
 const container = document.getElementById('.container')
-const colors = [']
+const colors = ['lightblue', 'lightgreen', 'lilac', 'violet', 'lightsalmon', 'lightpink', 'lightseagreen', 'lightsteelblue']
+
+const SQUARES = 1000
+
+for(let i = 0; i < SQUARES; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+    container.appendChild(square)
+
+    square.addEventListener('mouseover', () => setColor(square))
+}
+
+function setColor(element) {
+    const color = getRandomColor()
+    element.style.background = color
+}
+
+function getRandomColor() {
+    return colors[Math.floor(Math.random()*colors.length)]
+}
