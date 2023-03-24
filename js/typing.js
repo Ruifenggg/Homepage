8f6b8214e88421bffea6287d5078e77cdae65bda
Ruifeng Liang <111317718+Ruifenggg@users.noreply.github.com>
2023-03-24 16:42:28 +0000

    Fri, Mar 24, 2023, 4:42 PM +00:00

--- js/typing.js
+++ js/typing.js
@@ -2,3 +2,10 @@ const textEl = document.getElementById('text')
 const speedEl = document.getElementById('speed')
 const text = 'SHRIMP FRIED RICE SHALL RISE!'
 let idx = 1
+let speed = 300/speedEl.val
+
+writeText()
+
+function writeText() {
+    textEl.innerText = text.slice(0, )
+}
